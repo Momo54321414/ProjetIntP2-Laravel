3813968eb503df74af4d16e6f78c1723ae70c0b0
MoMo <kelian_berthelot@hotmail.com>
2024-02-20 10:53:01 -0500

    CSS + Page Doc

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -1,8 +1,18 @@
-@extends('_layout.app') @section('title','Nouveau Formulaire') @section('content')
+@extends('_layout.app') @section('title','Documentation') @section('content')
 
-<div id="container">
-    <div id="banner">
-        Test
+<div id="banner">
+    <input id="search" type="text" placeholder="Search">
+    <div id="grid">
+        <div id="items">
+            <a href="#test">Home</a>
+            <a>Configuration</a>
+            <a>Item</a>
+            <a>item</a>
+        </div>
+        <div>
+            <div id="title"> {{__('Dashboard_Banner_Head')}} </div>
+            <div id="desc"> {{__('Dashboard_Banner_Head')}} </div>
+        </div>
     </div>
 </div>
 
