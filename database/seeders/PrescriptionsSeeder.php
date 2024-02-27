31096043d6f53aee06b1b8d2a6ed9b043a580cf4
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2024-02-27 08:50:42 -0500

    Fix data date dans prescription

--- database/seeders/PrescriptionsSeeder.php
+++ database/seeders/PrescriptionsSeeder.php
@@ -17,8 +17,8 @@ class PrescriptionsSeeder extends Seeder
     {
         DB::table('prescriptions')->insert([[
             'nameOfPrescription' => 'Twice a day',
-            'dateOfPrescription' => fake()->date('Y-m-d','now'),
-            'dateOfStart' => fake()->date('Y-m-d','now'),
+            'dateOfPrescription' => Carbon::now(),
+            'dateOfStart' => Carbon::now()->addDays(fake()->numberBetween(1, 20)),
             'durationOfPrescriptionInDays' => fake()->numberBetween(1, 30),
             'frequencyBetweenDosesInHours' => fake()->numberBetween(1, 24),
             'frequencyPerDay' => fake()->numberBetween(1, 6),
@@ -29,8 +29,8 @@ class PrescriptionsSeeder extends Seeder
         ],
         [
             'nameOfPrescription' => 'My first prescription',
-            'dateOfPrescription' => fake()->date('Y-m-d','now'),
-            'dateOfStart' => fake()->date('Y-m-d','now'),
+            'dateOfPrescription' =>  Carbon::now(),
+            'dateOfStart' => Carbon::now()->addDays(fake()->numberBetween(1, 20)),
             'durationOfPrescriptionInDays' => fake()->numberBetween(1, 30),
             'frequencyBetweenDosesInHours' => fake()->numberBetween(1, 24),
             'frequencyPerDay' => fake()->numberBetween(1, 6),
@@ -41,8 +41,8 @@ class PrescriptionsSeeder extends Seeder
         ],
         [
             'nameOfPrescription' => 'Monday blues',
-            'dateOfPrescription' => fake()->date('Y-m-d','now'),
-            'dateOfStart' => fake()->date('Y-m-d','now'),
+            'dateOfPrescription' =>  Carbon::now(),
+            'dateOfStart' => Carbon::now()->addDays(fake()->numberBetween(1, 20)),
             'durationOfPrescriptionInDays' => fake()->numberBetween(1, 30),
             'frequencyBetweenDosesInHours' => fake()->numberBetween(1, 24),
             'frequencyPerDay' => fake()->numberBetween(1, 6),
@@ -53,8 +53,8 @@ class PrescriptionsSeeder extends Seeder
         ],
         [
             'nameOfPrescription' => 'Making it pillable',
-            'dateOfPrescription' => fake()->date('Y-m-d','now'),
-            'dateOfStart' => fake()->date('Y-m-d','now'),
+            'dateOfPrescription' =>  Carbon::now(),
+            'dateOfStart' => Carbon::now()->addDays(fake()->numberBetween(1, 20)),
             'durationOfPrescriptionInDays' => fake()->numberBetween(1, 30),
             'frequencyBetweenDosesInHours' => fake()->numberBetween(1, 24),
             'frequencyPerDay' => fake()->numberBetween(1, 6),
@@ -65,8 +65,8 @@ class PrescriptionsSeeder extends Seeder
         ],
         [
             'nameOfPrescription' => 'Intake of pills',
-            'dateOfPrescription' => fake()->date('Y-m-d','now'),
-            'dateOfStart' => fake()->date('Y-m-d','now'),
+            'dateOfPrescription' =>  Carbon::now(),
+            'dateOfStart' => Carbon::now()->addDays(fake()->numberBetween(1, 20)),
             'durationOfPrescriptionInDays' => fake()->numberBetween(1, 30),
             'frequencyBetweenDosesInHours' => fake()->numberBetween(1, 24),
             'frequencyPerDay' => fake()->numberBetween(1, 6),
