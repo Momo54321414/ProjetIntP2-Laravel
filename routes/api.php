6c28d39f4050983f02f63f672b697aa891314988
Francine <fmatho@yahoo.com>
2024-02-27 10:52:55 -0500

    gitflow-feature-stash: APILogin

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('login', 'UserController@login');
+Route::post('register', [UserController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', 'UserController@index');
+    Route::post('logout', 'UserController@logout');
 });
