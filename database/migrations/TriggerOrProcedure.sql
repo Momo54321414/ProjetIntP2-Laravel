00d7584da1529b677d61c0034a18bf276ed832ce
Jeanmi790 <102696659+Jeanmi790@users.noreply.github.com>
2024-02-28 10:58:24 -0500

    Y'a rien qui fonctionne yaaay

--- database/migrations/TriggerOrProcedure.sql
+++ database/migrations/TriggerOrProcedure.sql
@@ -6,8 +6,9 @@ BEGIN
     DECLARE DateOfStart DATE;
     DECLARE FrequencyPerDay INT;
     DECLARE DurationOfPrescription INT;
+    DECLARE HourOfIntake TIME;
+    DECLARE DateOfIntake DATE;
     DECLARE hours TIME;
-    DECLARE DateAndHour DATETIME;
     DECLARE ctrDurationDays INT;
 
     SELECT dateOfStart, frequencyPerDay, durationOfPrescriptionInDays INTO DateOfStart, FrequencyPerDay, DurationOfPrescription FROM prescriptions
@@ -21,9 +22,9 @@ BEGIN
             SET hours = SEC_TO_TIME(24 * 3600 / FrequencyPerDay);
         END IF;
 
-        SET DateAndHour = CONCAT(DateOfStart, ' ', hours);
-
-        INSERT INTO calendars (prescription_id, dateAndHour)
+        SET DateOfIntake = dateOfStart + INTERVAL ctrDurationDays DAY;
+        SET HourOfIntake = hours;
+        INSERT INTO calendars (prescription_id, d)
         VALUES (Prescription_id, DateAndHour);
         SET ctrDurationDays = ctrDurationDays + 1;
     END WHILE;
@@ -33,57 +34,72 @@ DELIMITER ;
 CALL insert_prescriptions_dates (2);
 DROP procedure insert_prescriptions_dates;
 
-
 DELIMITER //
-CREATE PROCEDURE test_column(IN DATE DATE,IN TIME TIME)
+CREATE PROCEDURE insert_prescriptions_dates(
+    IN Prescription_id INT,
+    OUT DateOfStart DATE,
+    OUT FrequencyPerDay INT,
+    OUT DurationOfPrescription INT
+)
 BEGIN
-DECLARE DATEANDHOUR DATETIME;
-SET DATEANDHOUR = CONCAT(DATE, ' ', TIME);
-SELECT DATEANDHOUR;
-END //
-DELIMITER ; USE laravel;
-
-DELIMITER //
-CREATE PROCEDURE insert_prescriptions_dates(IN prescription_id INT)
-BEGIN
-    DECLARE DateOfStart DATE;
-    DECLARE FrequencyPerDay INT;
-    DECLARE DurationOfPrescription INT;
+    -- Declare local variables (different names from OUT parameters)
+    DECLARE StartDateVar DATE;
+    DECLARE FrequencyVar INT;
+    DECLARE DurationVar INT;
+    DECLARE HourOfIntake TIME;
+    DECLARE DateOfIntake DATE;
     DECLARE hours TIME;
-    DECLARE DateAndHour DATETIME;
     DECLARE ctrDurationDays INT;
 
-    SELECT dateOfStart, frequencyPerDay, durationOfPrescriptionInDays INTO DateOfStart, FrequencyPerDay, DurationOfPrescription FROM prescriptions
+    -- Retrieve values from the prescriptions table
+    SELECT dateOfStart, frequencyPerDay, durationOfPrescriptionInDays INTO StartDateVar, FrequencyVar, DurationVar
+    FROM prescriptions
     WHERE id = Prescription_id;
 
-    SET ctrDurationDays = 0;
-    WHILE ctrDurationDays < DurationOfPrescription DO
-        IF FrequencyPerDay = 1 THEN
-            SET hours = '24:00:00';
-        ELSE
-            SET hours = SEC_TO_TIME(24 * 3600 / FrequencyPerDay);
-        END IF;
+    -- Set OUT parameters with local variables
+    SET DateOfStart = StartDateVar;
+    SET FrequencyPerDay = FrequencyVar;
+    SET DurationOfPrescription = DurationVar;
 
-        SET DateAndHour = STR_TO_DATE(CONCAT(dateOfStart,' ',hours),'%Y-%m-%d %H:%i:%s');
+    -- Uncomment and modify the following logic as needed
+    -- SET ctrDurationDays = 0;
+    -- WHILE ctrDurationDays < DurationOfPrescription DO
+    --     IF FrequencyPerDay = 1 THEN
+    --         SET hours = '24:00:00';
+    --     ELSE
+    --         SET hours = SEC_TO_TIME(24 * 3600 / FrequencyPerDay);
+    --     END IF;
 
-        INSERT INTO calendars (prescription_id, dateAndHour)
-        VALUES (Prescription_id, DateAndHour);
-        SET ctrDurationDays = ctrDurationDays + 1;
-    END WHILE;
+    --     SET HourOfIntake = hours;
+    --     INSERT INTO calendars (prescription_id, dateOfIntake, hourOfIntake)
+    --     VALUES (Prescription_id, DateOfIntake, HourOfIntake);
+
+    --     SET ctrDurationDays = ctrDurationDays + 1;
+    -- END WHILE;
+
 END //
 DELIMITER ;
+CALL insert_prescriptions_dates(2, @DateOfStart, @FrequencyPerDay, @DurationOfPrescription);
+SELECT @DateOfStart, @FrequencyPerDay, @DurationOfPrescription;
+DROP procedure insert_prescriptions_dates;
+
 
 DELIMITER //
-CREATE PROCEDURE test_column(IN date DATE,IN time TIME,OUT MYDATE DATETIME)
-BEGIN
-DECLARE DATEANDHOUR DATETIME;
-SET DATEANDHOUR = CAST(CONCAT(date, ' ', time) AS DATETIME);
-SELECT DATEANDHOUR INTO @MYDATE;
-END //
-DELIMITER ;
-CALL test_column('2023-02-12','10:10:10',@MyDATE);
-SELECT @DATEANDHOUR;
-DROP PROCEDURE test_column;
+CREATE PROCEDURE insert_prescriptions_dates(
+    IN Prescription_id INT,
+    OUT DateOfStart DATE,
+    OUT FrequencyPerDay INT,
+    OUT DurationOfPrescription INT
+)BEGIN
 
-CALL insert_prescriptions_dates (2);
-DROP procedure insert_prescriptions_dates;
+DECLARE returnDate DATE;
+DECLARE returnFrequency INT;
+
+
+  select date(dateOfStart) as date , frequencyPerDay as freq from prescriptions where id = idP;
+	 SET returnDate = str_to_date(@returnDate, '%Y-%m-%d');
+     SET returnFrequency = @returnFrequency;
+  
+END
+DELIMITER ;
+```
