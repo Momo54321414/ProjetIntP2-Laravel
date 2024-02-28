e8c6bcf8dda867a0a8a4ce42aa868bbd5bacf9c1
Jeanmi790 <102696659+Jeanmi790@users.noreply.github.com>
2024-02-27 23:23:49 -0500

    WIP
    Simplification de la table calendar

--- database/migrations/2024_02_12_122449_create_calendars_table.php
+++ database/migrations/2024_02_12_122449_create_calendars_table.php
@@ -12,10 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('calendars', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('prescription_id')->constrained('prescriptions');
+            $table->id();  
             $table->date('dateOfIntake');
             $table->time('hourOfIntake');
+            $table->foreignId('prescription_id')->constrained('prescriptions');
             $table->timestamps();
         });
     }
--- database/migrations/TriggerOrProcedure.sql
+++ database/migrations/TriggerOrProcedure.sql
@@ -41,4 +41,49 @@ DECLARE DATEANDHOUR DATETIME;
 SET DATEANDHOUR = CONCAT(DATE, ' ', TIME);
 SELECT DATEANDHOUR;
 END //
+DELIMITER ; USE laravel;
+
+DELIMITER //
+CREATE PROCEDURE insert_prescriptions_dates(IN prescription_id INT)
+BEGIN
+    DECLARE DateOfStart DATE;
+    DECLARE FrequencyPerDay INT;
+    DECLARE DurationOfPrescription INT;
+    DECLARE hours TIME;
+    DECLARE DateAndHour DATETIME;
+    DECLARE ctrDurationDays INT;
+
+    SELECT dateOfStart, frequencyPerDay, durationOfPrescriptionInDays INTO DateOfStart, FrequencyPerDay, DurationOfPrescription FROM prescriptions
+    WHERE id = Prescription_id;
+
+    SET ctrDurationDays = 0;
+    WHILE ctrDurationDays < DurationOfPrescription DO
+        IF FrequencyPerDay = 1 THEN
+            SET hours = '24:00:00';
+        ELSE
+            SET hours = SEC_TO_TIME(24 * 3600 / FrequencyPerDay);
+        END IF;
+
+        SET DateAndHour = STR_TO_DATE(CONCAT(dateOfStart,' ',hours),'%Y-%m-%d %H:%i:%s');
+
+        INSERT INTO calendars (prescription_id, dateAndHour)
+        VALUES (Prescription_id, DateAndHour);
+        SET ctrDurationDays = ctrDurationDays + 1;
+    END WHILE;
+END //
 DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE test_column(IN date DATE,IN time TIME,OUT MYDATE DATETIME)
+BEGIN
+DECLARE DATEANDHOUR DATETIME;
+SET DATEANDHOUR = CAST(CONCAT(date, ' ', time) AS DATETIME);
+SELECT DATEANDHOUR INTO @MYDATE;
+END //
+DELIMITER ;
+CALL test_column('2023-02-12','10:10:10',@MyDATE);
+SELECT @DATEANDHOUR;
+DROP PROCEDURE test_column;
+
+CALL insert_prescriptions_dates (2);
+DROP procedure insert_prescriptions_dates;
