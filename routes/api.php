e441415c94a0c024bdf408006ab5c5bae5169504
Francine <fmatho@yahoo.com>
2024-03-04 09:03:01 -0500

    dernier push avant clean project

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });*/
 
-Route::post('login', 'UserController@login');
+Route::post('login', [UserController::class,'login']);
 Route::post('register', [UserController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
