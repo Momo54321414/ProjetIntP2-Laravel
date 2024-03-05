2ae8dc5b6a9d16c7f459f70e862c11cf7ebf316a
antoineg94 <117280367+antoineg94@users.noreply.github.com>
2024-03-05 10:28:28 -0500

    frontend done

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -14,13 +14,6 @@ class LogController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -38,27 +31,4 @@ class LogController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
