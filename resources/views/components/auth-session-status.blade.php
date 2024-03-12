2af15304c904207b9f20eb1d3c27aee039afb1c3
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2024-03-12 14:43:57 -0400

    Affichage erroné

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,34 +1,33 @@
 @props(['status'])
 
 @php
-
-switch($status){
-case 'status':
-$bg = 'bg-green-100';
-$border = 'border-green-400';
-$text = 'text-green-700';
-$icon = 'text-green-500';
-break;
-case 'error':
-$bg = 'bg-red-100';
-$border = 'border-red-400';
-$text = 'text-red-700';
-$icon = 'text-red-500';
-break;
-case 'warning':
-$bg = 'bg-yellow-100';
-$border = 'border-yellow-400';
-$text = 'text-yellow-700';
-$icon = 'text-yellow-500';
-break;
-default:
-$bg = 'bg-teal-100';
-$border = 'border-teal-500';
-$text = 'text-teal-700';
-$icon = 'text-teal-500';
-break;
-
-}
+   
+    switch ($status) {
+        case 'status':
+            $bg = 'bg-green-100';
+            $border = 'border-green-400';
+            $text = 'text-green-700';
+            $icon = 'text-green-500';
+            break;
+        case 'errors':
+            $bg = 'bg-red-100';
+            $border = 'border-red-400';
+            $text = 'text-red-700';
+            $icon = 'text-red-500';
+            break;
+        case 'warning':
+            $bg = 'bg-yellow-100';
+            $border = 'border-yellow-400';
+            $text = 'text-yellow-700';
+            $icon = 'text-yellow-500';
+            break;
+        default:
+            $bg = 'bg-teal-100';
+            $border = 'border-teal-500';
+            $text = 'text-teal-700';
+            $icon = 'text-teal-500';
+            break;
+    }
 @endphp
 
 
@@ -40,9 +39,11 @@ break;
     </span>
 </div> --}}
 
-    <div class="  {{$bg}} border-t-4 {{$border}} rounded-b {{$text}} px-4 py-3 shadow-md  " role="alert">
+    <div class="  {{ $bg }} border-t-4 {{ $border }} rounded-b {{ $text }} px-4 py-3 shadow-md  "
+        role="alert">
         <div class="py-1">
-            <svg class="fill-current h-6 w-6 {{$icon}} mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <svg class="fill-current h-6 w-6 {{ $icon }} mr-4" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20">
                 <path
                     d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
             </svg>
