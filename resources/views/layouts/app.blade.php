60fe16181443631b4ee22c5af210271e3f99c9a3
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2024-03-18 09:06:34 -0400

    Ajout icon pour app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="icon" href="{{ asset('img/App_Logo.svg') }}" type="image/x-icon" />
 
 
     <!-- Scripts -->
