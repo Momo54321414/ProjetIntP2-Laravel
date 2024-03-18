1f94454ace03afdd13160442f15c09c541abd53b
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2024-03-18 08:58:57 -0400

    Changement du nom du projet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Pillbox') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -34,7 +34,7 @@
         <div class= " max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
 
             @include('components.auth-session-status', ['status' => session('status')])
-            
+
         </div>
         <!-- Page Content -->
         <main>
